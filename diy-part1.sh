06e03d8952eec48823888de4dd077e652ef33182
imacer <84390587+imacer@users.noreply.github.com>
2026-03-16 18:42:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-# 如果你需要额外的插件源，在这里添加
-# echo 'src-git extra https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+#
+# Copyright (c) 2019-2026 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 1. (可选) 添加第三方插件源
+# 如果官方源的插件不满足你，可以取消下面这一行的注释来添加常用的大神源
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+
+# 2. 确保使用官方 25.12 的核心源 (通常源码自带，此处为双重保险)
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 3. 示例：如果你有特定的私有包，可以在这里添加
+# echo 'src-git my_packages https://github.com/your_username/my_repo' >> feeds.conf.default
+
+echo "DIY-Part1 脚本执行完毕，插件源已就绪。"
